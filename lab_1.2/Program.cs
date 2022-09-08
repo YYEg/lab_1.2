638eb9d4d4b64a67f62962e930536e52777f5f58
YuEgor <rezervdesu15@gmail.com>
2022-09-08 20:50:57 +0800

    Добавьте файлы проекта.

---
+++ lab_1.2/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace FirstApplication
+{
+        
+    public class Logic
+    {
+        public static string Check(List<int> list)
+        {
+            string outMessage = "";
+            bool IsPositive = true;
+            if (list[0] < 0)
+            {
+                IsPositive = false;
+            }
+            int changeCounter = 0;
+            foreach (var checkEl in list)
+            {
+                if ((IsPositive) && (checkEl < 0))
+                {
+                    changeCounter++;
+                    IsPositive = false;
+                }
+                else if ((!IsPositive) && (checkEl > 0))
+                {
+                    changeCounter++;
+                    IsPositive = true;
+                }
+            }
+            outMessage = $"Знак изменился {changeCounter} раз";
+            return outMessage; 
+        }
+    }
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //НАЧАЛО взаимодейтсвия с пользователем
+            Console.WriteLine("Введите колличество членов массива:");
+            var arraySize = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите элементы массива");
+
+            List<int> list = new List<int>(arraySize);
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.WriteLine($"Введите {i+1} элемент массива:");
+                var listEl = int.Parse(Console.ReadLine());
+                list.Add(listEl);
+            }
+
+            Console.WriteLine("Ваш массив:");
+            foreach (var el in list)
+            {
+                Console.WriteLine(el);
+                
+            }
+            //КОНЕЦ взаимодействия с пользователем
+
+            //НАЧАЛО логики
+            var outMessage = Logic.Check(list);
+            //КОНЕЦ логики
+                            
+            //НАЧАЛО взаимодейтсвия с пользователем
+            Console.WriteLine(outMessage);
+            Console.ReadLine();
+            //КОНЕЦ взаимодейтсвия с пользователем
+        }
+    }
+}
+
---
+++ lab_1.2Tests/LogicTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void SignChanges2Times()
+        {
+            List<int> list = new List<int>() { 1, -1, 2, 4};
+
+            var outMessage = Logic.Check(list);
+
+            Assert.AreEqual("Знак изменился 2 раз", outMessage );
+        }
+
+        [TestMethod()]
+        public void SignChanges3Times()
+        {
+            List<int> list = new List<int>() { 1, -1, 2, -4};
+
+            var outMessage = Logic.Check(list);
+
+            Assert.AreEqual("Знак изменился 3 раз", outMessage);
+        }
+
+        [TestMethod()]
+        public void SignChanges1Times()
+        {
+            List<int> list = new List<int>() { 1, -1};
+
+            var outMessage = Logic.Check(list);
+
+            Assert.AreEqual("Знак изменился 1 раз", outMessage);
+        }
+    }
+}
